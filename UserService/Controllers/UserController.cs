adccd07908b7b1490e7b7c565573ff20b6ae76d7
Ruslan <crosssimply@gmail.com>
2026-04-09 17:50:03 +0300

    Created base of UserServiceAPI.

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace UserServiceAPI.Controllers
 {
-    internal class Class1
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
+        
     }
 }
